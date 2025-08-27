35a8d356b6c828aa8e13ae8067230787ccc7de7d
Raja Muhammad Farhan Zahputra <raja.farhan0503@gmail.com>
2025-08-27 22:47:19 +0700

    style: add custom font styles and color

--- app/src/main/java/com/rajafarhan/expensetracker/presentation/theme/Type.kt
+++ app/src/main/java/com/rajafarhan/expensetracker/presentation/theme/Type.kt
@@ -2,14 +2,29 @@ package com.rajafarhan.expensetracker.presentation.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.rajafarhan.expensetracker.R
+
+
+val Inter = FontFamily(
+    Font(R.font.inter_bold, FontWeight.Bold),
+    Font(R.font.inter_thin, FontWeight.Thin),
+    Font(R.font.inter_black, FontWeight.Black),
+    Font(R.font.inter_light, FontWeight.Light),
+    Font(R.font.inter_medium, FontWeight.Medium),
+    Font(R.font.inter_semi_bold, FontWeight.SemiBold),
+    Font(R.font.inter_regular, FontWeight.Normal),
+    Font(R.font.inter_extra_light, FontWeight.ExtraLight),
+    Font(R.font.inter_extra_bold, FontWeight.ExtraBold)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Inter,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
